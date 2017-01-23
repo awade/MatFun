d33b57852a6b2b49d7738056bed72810dea77432
Andrew Wade <andrew.richard.wade@gmail.com>
2017-01-23 12:34:50 -0800

    Standard template for new functions

---
+++ function_template.m
@@ -0,0 +1,40 @@
+function [output1,output2] = function_template(input1,input2,input3)
+%FUNCTION_NAME - One line description of what the function or script performs (H1 line)
+%Optional file header info (to give more details about the function than in the H1 line)
+%Optional file header info (to give more details about the function than in the H1 line)
+%Optional file header info (to give more details about the function than in the H1 line)
+%
+% Syntax:  [output1,output2] = function_name(input1,input2,input3)
+%
+% Inputs:
+%    input1 - Description
+%    input2 - Description
+%    input3 - Description
+%
+% Outputs:
+%    output1 - Description
+%    output2 - Description
+%
+% Example: 
+%    Line 1 of example
+%    Line 2 of example
+%    Line 3 of example
+%
+% Other m-files required: none
+% Subfunctions: none
+% MAT-files required: none
+%
+% See also: OTHER_FUNCTION_NAME1,  OTHER_FUNCTION_NAME2
+
+% Author: Andrew Wade
+% Work address: West-bridge Caltech
+% email: awade@ligo.caltech.edu
+% Website: 
+% Month 2017; Last revision: xx-Month-YYYY
+
+%------------- BEGIN CODE --------------
+
+Enter your executable matlab commands here
+
+%------------- END OF CODE --------------
+%Please send suggestions/questions to address at top of script
