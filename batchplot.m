e5d894727dc62508b33ceba0467beb887d16dbc4
Andrew Wade <andrew.richard.wade@gmail.com>
2017-01-23 19:59:43 -0800

    Removed elements of script now handled by bplotdual.m

--- batchplot.m
+++ batchplot.m
@@ -19,53 +19,15 @@ importList = {
 'PSDNoiseIntergratorCircuitTMTF_13-01-2017_111010.txt','dataTag',30,2,'Plot title','channelOneTitle','ChannelTwoTitle','xaxislabel','yaxislabel','annotationch1','annotationch2','comment';...
 };
 
-importList ={'PSDNoiseIntergratorCircuitTMTF_13-01-2017_111010.txt','intNoise',30,2,'Plot all title','channelOneTitle','ChannelTwoTitle','xaxislabel','yaxislabelCh1','yaxislabelCh2','annotation1','anotation2','comment'}
+importList ={'PSDNoiseIntergratorCircuitTMTF_13-01-2017_111010.txt','intNoise',30,2,'Plot all title','channelOneTitle','ChannelTwoTitle','xaxislabel','yaxislabelCh1','yaxislabelCh2','annotation1','anotation2','comment';...
+    'PSDNoiseIntergratorCircuitTMTF_13-01-2017_111010.txt','test2part',30,2,'Plot all title','channelOneTitle','ChannelTwoTitle','xaxislabel','yaxislabelCh1','yaxislabelCh2','annotation1','anotation2','comment'};
 
 s = import2struct(importList); % Generate blank array to be added to below
 
 
 
-%% Generate plots by data bin
 
-%Standard two pannel plot displaying one set of data (for each channel for
-%each plot
-startFigNum = 1;
-for n = 1:length(s.MetaList)
-    figure(startFigNum - 1 + n)
-    subplot(2,1,1)
-    plot(s.(char(s.MetaList(n))).fStitched,s.(char(s.MetaList(n))).ch1Stitched)
-    ax = gca; % Returns handle of the curret axes for the current figure
-    ax.GridLineStyle = '-'; % Sets grid lines to solid instead of defult dotted
-    ax.MinorGridLineStyle = '-'; % Sets minor grid lines to solid instead of defult dotted
-    ax.XScale = 'log'; %Sets axis type to 'log'
-    ax.XScale = 'log';
-    ax.FontSize = 14; % Set the font size to something readable
-    % % ax.YLim = [-170,-80]; % Set y-axis limits
-    ax.XLim = [min(min(s.(char(s.MetaList(n))).fStitched)),max(max(s.(char(s.MetaList(n))).fStitched))];
-    title(s.(char(s.MetaList(n))).Ch1Title)
-    xlabel(s.(char(s.MetaList(n))).xaxisLabel)
-    ylabel(s.(char(s.MetaList(n))).yaxisLabelCh1)
-    legend(s.(char(s.MetaList(n))).legendch1)
-    grid on
-    
-    subplot(2,1,2)
-    plot(s.(char(s.MetaList(n))).fStitched,s.(char(s.MetaList(n))).ch2Stitched)
-    % eval(['plot(' PlotEntryListPha ')']) % Plots list of entries created in do loop above, eval is useful for dynamic lines of code
-    ax = gca; % Returns handle of the curret axes for the current figure
-    ax.GridLineStyle = '-'; % Sets grid lines to solid instead of defult dotted
-    ax.MinorGridLineStyle = '-'; % Sets minor grid lines to solid instead of defult dotted
-    ax.XScale = 'log'; %Sets axis type to 'log'
-    ax.YScale = 'log';
-    ax.FontSize = 14; % Set the font size to something readable
-    % % ax.YLim = [-170,-80]; % Set y-axis limits
-    ax.XLim = [min(min(s.(char(s.MetaList(n))).fStitched)),max(max(s.(char(s.MetaList(n))).fStitched))];
-    title(s.(char(s.MetaList(n))).Ch1Title)
-    xlabel(s.(char(s.MetaList(n))).xaxisLabel)
-    ylabel(s.(char(s.MetaList(n))).yaxisLabelCh2)
-    legend(s.(char(s.MetaList(n))).legendch2)
-    grid on
-    hold off
-end
+[h] = bplotdual(s);
 
 
 %%
